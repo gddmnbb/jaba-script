42eb78c19b6d9cf984f1f51413721dce1cf9c49e
Олёна Филатова <gddmnbb@Olenas-Air.w.mifi>
2023-05-15 18:05:10 +0300

    хуй

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-let title, screens, screenPrice, rollback, fullPrice, adaptive;
-alert('что за тяги');
-console.log('такие бархатные тяги');
+let title = 'тяги';
+let screens = 'Простые, Сложные, Интерактивные';
+let screenPrice = 228;
+let rollback = 69;
+let fullPrice = 1000000;
+let adaptive = false;
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+console.log(`Стоимость верстки экранов ${screenPrice} гривен`);
+console.log(`Стоимость разработки сайтов ${fullPrice} гривен`);
+console.log(screens.toLocaleLowerCase().split(', '));
+
+console.log(fullPrice * (rollback / 100));
