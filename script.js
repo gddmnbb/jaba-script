fddb06802bc1fc2e494139de89f09c7f83fa279e
Олёна Филатова <gddmnbb@Olenas-Air.w.mifi>
2023-05-17 18:54:14 +0300

    lesson 7

--- script.js
+++ script.js
@@ -1,48 +1,61 @@
-let title = prompt('Как называется ваш проект?');
-let screens = prompt('Какие типы экранов нужно разработать?');
-let screenPrice = parseInt(prompt('Сколько будет стоить данная работа'));
-let rollback = 69;
-let adaptive = prompt('Нужен ли адаптив на сайте?').toLocaleLowerCase() === "да" ? true : false;
+const appData = {
+    title: '',
+    screens: '',
+    screenPrice: 0,
+    rollback: 69,
+    adaptive: false,
+    servicePrice: 0,
 
-let servicePrice = 0;
+    getAllServicePrices() {
+        return this.servicePrice;
+    },
+    // getAllServicePrices: this.servicePrice,
+    getFullPrice() {
+        return this.screenPrice + this.getAllServicePrices();
+    },
+    getServicePercentPrices() {
+        return parseInt(this.fullPrice - this.fullPrice * (this.rollback / 100));
+    },
 
-// Serivce
-// let [service1, serivcePrice1] = [prompt('Какой дополнительный тип услуги нужен?'), parseInt(prompt('Сколько это будет стоить?'))];
-// let [service2, serivcePrice2] = [prompt('Какой дополнительный тип услуги нужен?'), parseInt(prompt('Сколько это будет стоить?'))];
+    getTitle: () => this.title.trim().charAt(0).toUpperCase() + this.title.trim().slice(1),
 
-let userInput;
-do {
-    userInput = prompt('Сколько это будет стоить?');
-    if(userInput && !isNaN(userInput)) servicePrice += parseInt(userInput);
-} while (userInput);
+    getRollbackMessage: () => {
+        let sale;
+        if(this.fullPrice >= 30000) sale = 10;
+        else if(30000 > this.fullPrice >= 15000) sale = 5;
+        else if(15000 > this.fullPrice) sale = 0;
+        else if(this.fullPrice < 0) sale = -1;
 
-const getAllServicePrices = () => servicePrice;
-const getFullPrice = () => screenPrice + getAllServicePrices();
-const getServicePercentPrices = () => parseInt(getFullPrice() - getFullPrice() * (rollback / 100));
+        if(sale === -1)
+            return 'дурак что ле';
+        else if(sale === 0)
+            return 'Скидка не предусмотрена';
+        else
+            return `Даем скидку ${sale}%`;
+    },
 
-let allServicePrices = getAllServicePrices();
-let fullPrice = getFullPrice();
-let servicePercentPrice = getServicePercentPrices();
+    asking() {
+        let userInput;
+        do {
+            userInput = prompt('Сколько это будет стоить?');
+            if(userInput && !isNaN(userInput)) this.servicePrice += parseInt(userInput);
+        } while (userInput);
+    },
 
-const getTitle = () => title.trim().charAt(0).toUpperCase() + title.trim.slice(1);
+    start() {
+        this.title = prompt('Как называется ваш проект?');
+        this.screens = prompt('Какие типы экранов нужно разработать?');
+        this.screenPrice = parseInt(prompt('Сколько будет стоить данная работа'));
+        this.adaptive = prompt('Нужен ли адаптив на сайте?').toLocaleLowerCase() === "да" ? true : false;
 
-const getRollbackMessage = () => {       
-    let sale;
-    if(fullPrice >= 30000) sale = 10;
-    else if(30000 > fullPrice >= 15000) sale = 5;
-    else if(15000 > fullPrice) sale = 0;
-    else if(fullPrice < 0) sale = -1;
+        this.asking();
+        this.logger();
+    },
 
-    if(sale === -1)
-        return 'дурак что ле';
-    else if(sale === 0)
-        return 'Скидка не предусмотрена';
-    else
-        return `Даем скидку ${sale}%`;
+    logger() {
+        for (const key in this)
+            console.log(key);
+    }
 }
 
-// Log zone
-// console.log(show);
-console.log(screens.split(','));
-console.log(getRollbackMessage());
-console.log(getServicePercentPrices());
+appData.start();
